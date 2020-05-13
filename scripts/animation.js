5391a646e1b0d0697235ae6e06969463926a2cf4
eeinoder <nicolaeeinoder@gmail.com>
2020-05-13 15:36:09 -0700

    Bug fix: resizing window no longer breaks animation

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,10 +1,15 @@
 // Document ready: slide "Einoder" right and fade in AND move down "Welcome" text
 // Scroll effects/animations:
-const gOffset = window.innerWidth / 300; // 1% of viewport width
+
+const gOffset = window.innerWidth / 300; // 1% of viewport width in pixels
+const initialMargins = ["6vw", "15.9vw", "24vw", "0vw", "11.5vw", "22.5vw"];
 var lastScrollVal = 0; // after some elapsed time, calculate largest scroll value in burst (?)
-var maxScrollVal = 0;
+var bufferLeft = 0; //
+var bufferRight = 0
 
 $(document).ready(function() {
+  setMargins();
+
   $('p').animate({opacity:1},4000);
   //$('h2').animate({opacity:1},{queue:false,duration:600}).animate({right:'15vw'},{duration:1000});
   $(window).bind('mousewheel DOMMouseScroll', function(e){
@@ -22,49 +27,52 @@ $(document).ready(function() {
 
 
 
-// Scroll handler - Attempt 2: motion is binded to 'scroll wheel inside div'
+// Scroll handler - Attempt 2: motion is binded to 'scroll wheel position inside div'
+// Theory: this will make it work in iOS / mobile
 
 
 
 // Scroll handler - Attempt 1: motion is binded to 'mouse wheel'
-// If scroll offset is positive (i.e. scroll up), move forward in animation by 1 gOffset.
-// If scroll offset is negative (i.e. scroll down),  move backward by 1 gOffset
 // Note: better execution if offset is not static value. Rather if offset proportional to scroll value.
-// Note: we manipulate the 'left-meargin' of both
+// Note: we manipulate the 'left-margin' of both name objects
 function scrollAnimation(scrollDirection) {
-  var fixedOffset = -1 * Math.sign(scrollDirection) * gOffset;
-  var propOffSet = -1 * (scrollDirection/100) * gOffset;
-  var offset = propOffSet;
+  var offset = -1 * (scrollDirection/100) * gOffset;
   //console.log(offset);
   var i;
-  var lTarget, rTarget;
-  var oldLastnmMrgStr, oldLastnmMrgFlt, newLastnmMrgStr, newLastnmMrgFlt;
-  var oldFirstnmMrgStr, oldFirstnmMrgFlt, newFirstnmMrgStr, newFirstnmMrgFlt;
   for (i=0; i<3; i++) { // Works because each name is split into 3 units
     // Parse string -> to int -> add value -> to string -> append 'px' to end
     lTarget = '#lastname' + (i+1);
-    rTarget = '#firstname' + (i+1);
-    // Handle last name shift
-    oldLastnmMrgStr = $(lTarget).css("margin-left");
-    oldLastnmMrgFlt = parseFloat(oldLastnmMrgStr.substr(0,oldLastnmMrgStr.length-2)); // from '...px'
-    newLastnmMrgFlt = oldLastnmMrgFlt + offset;
-    newLastnmMrgStr = newLastnmMrgFlt + "px";
-    $(lTarget).css("margin-left", newLastnmMrgStr);
-    if (i==2) {console.log("Last: " + newLastnmMrgStr);}
-    // Handle first name shift
-    oldFirstnmMrgStr = $(rTarget).css("margin-left");
-    oldFirstnmMrgFlt = parseFloat(oldFirstnmMrgStr.substr(0,oldFirstnmMrgStr.length-2)); // from '...px'
-    newFirstnmMrgFlt = oldFirstnmMrgFlt - offset;
-    newFirstnmMrgStr = newFirstnmMrgFlt + "px";
-    $(rTarget).css("margin-left", newFirstnmMrgStr);
-    if (i==0) {console.log("First: " + newFirstnmMrgStr);}
+    fTarget = '#firstname' + (i+1);
+    // Handle last name shift: px -> vw, set new value
+    newMarginLastnm = 100*(getMargin(lTarget) + offset)/window.innerWidth + "vw";
+    $(lTarget).css("margin-left", newMarginLastnm);
+    // Handle first name shift: px -> vw, set new value
+    newMarginFirstnm = 100*(getMargin(fTarget) - offset)/window.innerWidth + "vw";
+    $(fTarget).css("margin-left", newMarginFirstnm);
   }
-  console.log('\n');
+}
+ /* Helper: return float value of left-margin in pixels  */
+function getMargin(targetName) {
+  var margin = $(targetName).css("margin-left");
+  return parseFloat(margin.substr(0,margin.length-2));
 }
 
-
-
-
+/* Helper: set values for left-margin. If no margin array given, use global initialMargins, */
+function setMargins(newMargins) {
+  var i;
+  var lTarget, fTarget;
+  var margins = initialMargins;
+  //console.log(newMargins)
+  if (newMargins !== undefined && newMargins !== null) {
+    margins = newMargins; // assume newMargins is array with 6 strings, 'Xvw'
+  }
+  for (i=0; i<3; i++) {
+    lTarget = '#lastname' + (i+1);
+    fTarget = '#firstname' + (i+1);
+    $(fTarget).css("margin-left", margins[i]);
+    $(lTarget).css("margin-left", margins[i+3]);
+  }
+}
 
 
 
